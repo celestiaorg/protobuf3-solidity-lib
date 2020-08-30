454d20ad527c3d560fd82f46b9e6bad6c909a7c1
John Adler <john.adler@protonmail.ch>
2020-08-30 17:49:47 -0400

    Fix test fixture to use library functions instead of fixture functions.

--- contracts/TestFixture.sol
+++ contracts/TestFixture.sol
@@ -13,58 +13,58 @@ contract TestFixture {
             ProtobufLib.WireType
         )
     {
-        return decode_key(p, buf);
+        return ProtobufLib.decode_key(p, buf);
     }
 
     function decode_varint(uint256 p, bytes memory buf) public pure returns (uint256, uint64) {
-        return decode_varint(p, buf);
+        return ProtobufLib.decode_varint(p, buf);
     }
 
     function decode_uint32(uint256 p, bytes memory buf) public pure returns (uint256, uint32) {
-        return decode_uint32(p, buf);
+        return ProtobufLib.decode_uint32(p, buf);
     }
 
     function decode_uint64(uint256 p, bytes memory buf) public pure returns (uint256, uint64) {
-        return decode_uint64(p, buf);
+        return ProtobufLib.decode_uint64(p, buf);
     }
 
     function decode_bool(uint256 p, bytes memory buf) public pure returns (uint256, bool) {
-        return decode_bool(p, buf);
+        return ProtobufLib.decode_bool(p, buf);
     }
 
     function decode_enum(uint256 p, bytes memory buf) public pure returns (uint256, uint64) {
-        return decode_enum(p, buf);
+        return ProtobufLib.decode_enum(p, buf);
     }
 
     function decode_bits64(uint256 p, bytes memory buf) public pure returns (uint256, uint64) {
-        return decode_bits64(p, buf);
+        return ProtobufLib.decode_bits64(p, buf);
     }
 
     function decode_fixed64(uint256 p, bytes memory buf) public pure returns (uint256, uint64) {
-        return decode_fixed64(p, buf);
+        return ProtobufLib.decode_fixed64(p, buf);
     }
 
     function decode_bits32(uint256 p, bytes memory buf) public pure returns (uint256, uint32) {
-        return decode_bits32(p, buf);
+        return ProtobufLib.decode_bits32(p, buf);
     }
 
     function decode_fixed32(uint256 p, bytes memory buf) public pure returns (uint256, uint32) {
-        return decode_fixed32(p, buf);
+        return ProtobufLib.decode_fixed32(p, buf);
     }
 
     function decode_length_delimited(uint256 p, bytes memory buf) public pure returns (uint256, bytes memory) {
-        return decode_length_delimited(p, buf);
+        return ProtobufLib.decode_length_delimited(p, buf);
     }
 
     function decode_string(uint256 p, bytes memory buf) public pure returns (uint256, string memory) {
-        return decode_string(p, buf);
+        return ProtobufLib.decode_string(p, buf);
     }
 
     function decode_bytes(uint256 p, bytes memory buf) public pure returns (uint256, bytes memory) {
-        return decode_bytes(p, buf);
+        return ProtobufLib.decode_bytes(p, buf);
     }
 
     function decode_embedded_message(uint256 p, bytes memory buf) public pure returns (uint256, bytes memory) {
-        return decode_embedded_message(p, buf);
+        return ProtobufLib.decode_embedded_message(p, buf);
     }
 }
