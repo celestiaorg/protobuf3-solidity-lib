ba3881bd6b7f640eb28bee471fe61599b5926af3
John Adler <john.adler@protonmail.ch>
2020-08-30 20:23:36 -0400

    Add more unit tests.

--- test/unit.js
+++ test/unit.js
@@ -47,13 +47,81 @@ contract("TestFixture", async (accounts) => {
   it("[decode] uint32", async () => {
     const instance = await TestFixture.deployed();
 
+    const v = 300;
+
     const Message = new protobuf.Type("Message").add(new protobuf.Field("field", 1, "uint32"));
-    const message = Message.create({ field: 300 });
+    const message = Message.create({ field: v });
     const encoded = Message.encode(message).finish().toString("hex");
 
     const result = await instance.decode_uint32.call(1, "0x" + encoded);
     const { 0: pos, 1: val } = result;
     assert.equal(pos, 3);
-    assert.equal(val, 300);
+    assert.equal(val, v);
+  });
+
+  it("[decode] uint32 max", async () => {
+    const instance = await TestFixture.deployed();
+
+    const v = 4294967295;
+
+    const Message = new protobuf.Type("Message").add(new protobuf.Field("field", 1, "uint32"));
+    const message = Message.create({ field: v });
+    const encoded = Message.encode(message).finish().toString("hex");
+
+    const result = await instance.decode_uint32.call(1, "0x" + encoded);
+    const { 0: pos, 1: val } = result;
+    assert.equal(pos, 6);
+    assert.equal(val, v);
+  });
+
+  it("[decode] uint32 too large", async () => {
+    const instance = await TestFixture.deployed();
+
+    const v = 4294967296;
+
+    const Message = new protobuf.Type("Message").add(new protobuf.Field("field", 1, "uint64"));
+    const message = Message.create({ field: v });
+    const encoded = Message.encode(message).finish().toString("hex");
+
+    await truffleAssert.fails(instance.decode_uint32.call(1, "0x" + encoded));
+  });
+
+  it("[decode] uint64", async () => {
+    const instance = await TestFixture.deployed();
+
+    const v = 4294967296;
+
+    const Message = new protobuf.Type("Message").add(new protobuf.Field("field", 1, "uint64"));
+    const message = Message.create({ field: v });
+    const encoded = Message.encode(message).finish().toString("hex");
+
+    const result = await instance.decode_uint64.call(1, "0x" + encoded);
+    const { 0: pos, 1: val } = result;
+    assert.equal(pos, 6);
+    assert.equal(val, v);
+  });
+
+  it("[decode] uint64 max", async () => {
+    const instance = await TestFixture.deployed();
+
+    const v = "18446744073709551615";
+
+    const Message = new protobuf.Type("Message").add(new protobuf.Field("field", 1, "uint64"));
+    const message = Message.create({ field: v });
+    const encoded = Message.encode(message).finish().toString("hex");
+
+    const result = await instance.decode_uint64.call(1, "0x" + encoded);
+    const { 0: pos, 1: val } = result;
+    assert.equal(pos.toNumber(), 11);
+    assert.equal(val, v);
+  });
+
+  it("[decode] uint64 too large", async () => {
+    const instance = await TestFixture.deployed();
+
+    await truffleAssert.reverts(
+      instance.decode_uint64.call(1, "0x08ffffffffffffffffffff01"),
+      "varint used more than MAX_VARINT_BYTES bytes"
+    );
   });
 });
