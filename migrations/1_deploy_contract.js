47338a800916b75d91a9b2bdc91301343c1082ac
John Adler <john.adler@protonmail.ch>
2020-08-31 00:26:04 -0400

    Remove redundant linking of library.

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,8 +1,5 @@
-const ProtobufLib = artifacts.require("ProtobufLib");
 const TestFixture = artifacts.require("TestFixture");
 
 module.exports = function (deployer) {
-  deployer.deploy(ProtobufLib);
-  deployer.link(ProtobufLib, TestFixture);
   deployer.deploy(TestFixture);
 };
